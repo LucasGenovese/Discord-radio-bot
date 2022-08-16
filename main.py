57b1643ab1e177e90085784edf28deffc5e5decc
Lucas Genovese Desktop <lucas59311@gmail.com>
2022-08-16 00:57:07 -0300

    Bot commands with working radio

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'MTAwODg5ODEyMDY4OTY2MDA2NA.GU6TwT.tScKSU39olqXR_YX-N36Nhdo17Efv_HDQ5hlzQ'
+client = commands.Bot(command_prefix='.')
+
+@client.event
+async def on_ready():
+    print('Bot online.')
+
+@client.command()
+async def disconnect(ctx):
+    await ctx.voice_client.disconnect()
+
+@client.command() 
+async def join(ctx):
+    if ctx.author.voice is None:
+        await ctx.send("You are not in a voice channel!")
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+        await voice_channel.connect()
+    else:
+        await ctx.voice_client.move_to(voice_channel)
+
+@client.command()
+async def play(ctx):
+    voice_channel = ctx.author.voice.channel
+    vc = await voice_channel.connect()
+    vc.play(discord.FFmpegPCMAudio('https://listen1.myradio24.com/8234'), after=lambda e: print('done', e))
+
+client.run(TOKEN)
